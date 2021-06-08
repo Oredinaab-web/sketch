d0b19a37fff62ce9220df86d0452c98322978f40
Oredinaab-web <miguelalfonso140@gmail.com>
2021-06-08 10:43:49 +0800

    Alfie Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LoginComponent,
+  },
+  {
+    path: "main",
+    loadChildren: () => import("./main/main.module").then((m) => m.MainModule),
+    // path: "main",
+    // loadChildren: "./main/main.module#MainModule"
+  },
 
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
